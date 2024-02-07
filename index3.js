fa3499d984999f628461ece55c5cd78dadfa49c2
GopinathN15 <157477337+GopinathN15@users.noreply.github.com>
2024-02-07 14:58:49 +0530

    loop

---
+++ index3.js
@@ -0,0 +1,20 @@
+// var a =10;
+// var b=20;
+// var c = "20";
+// console.log(a*b);
+// console.log(typeof(c));
+// var age = null
+// console.log(age);
+ for(var i=0;i<=8;i++){
+
+    for(var j=0;j<=8;j++){
+
+      if((i+j%2)==0){
+         console.log("white");
+
+      }
+      else{
+         console.log("red");
+      }
+    }
+ }
